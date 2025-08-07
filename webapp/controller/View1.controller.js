5229c0ff97343177a31c4e795317ee3e601b0700
Brandon Kyle Dela Cruz <brandon.k.dela.cruz@accenture.com>
2025-08-07 13:17:38 +0000

    function finished

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,6 +5,42 @@ sap.ui.define([
 
     return Controller.extend("sapui5exercise1.controller.View1", {
         onInit() {
+
+        },
+
+        onClearPress: function(){
+            var oView = this.getView();
+
+            var userNameInput    = oView.byId("user_name");
+            var userStreetInput  = oView.byId("user_street");
+            var userNoInput      = oView.byId("user_no");
+            var userZipCodeInput = oView.byId("user_zip_code");
+            var userCityInput    = oView.byId("user_city");
+            var countrySelect    = oView.byId("country_select");
+            
+            if(userNameInput){
+                userNameInput.setValue("");
+            }
+
+            if(userStreetInput){
+                userStreetInput.setValue("");
+            }
+            
+            if(userNoInput){
+                userNoInput.setValue("");
+            }
+
+            if(userZipCodeInput){
+                userZipCodeInput.setValue("");
+            }
+
+            if(userCityInput){
+                userCityInput.setValue("");
+            }
+
+            if(countrySelect){
+                countrySelect.setSelectedKey("");
+            }
         }
     });
 });
